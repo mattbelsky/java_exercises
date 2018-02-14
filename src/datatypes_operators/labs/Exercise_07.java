2272576e0a13d374f4943540b3cc10cbafbc59f8
Matthew <matt.belsky@gmail.com>
2018-02-14 13:00:35 +0800

    push to matt_remote

--- src/datatypes_operators/labs/Exercise_07.java
+++ src/datatypes_operators/labs/Exercise_07.java
@@ -7,6 +7,24 @@ package datatypes_operators.labs;
  *      "for-each" loop, iterate and print out each element of the array.
  *
  */
-
 public class Exercise_07 {
+    public static void main(String[] args) {
+        // Countries beginning with "A" sorted into a nested array by region
+        String[][] countries = {
+                {"Afghanistan", "Armenia", "Azerbaijan"},
+                {"Albania", "Andorra", "Austria"},
+                {"Algeria", "Angola"},
+                {"American Samoa", "Australia"},
+                {"Anguilla", "Antigua & Barbuda", "Aruba"},
+                {"Antarctica"}
+        };
+
+        for (String country[] : countries) {
+            for (String c : country) {
+                System.out.println(c + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
