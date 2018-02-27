d1383865c20cc7fa6f1c27c95a6140410b3209e6
Matthew <matt.belsky@gmail.com>
2018-02-27 23:21:38 +0800

    push to matt_remote

--- src/generics/labs/Exercise_01.java
+++ src/generics/labs/Exercise_01.java
@@ -7,31 +7,103 @@ package generics.labs;
  *      2.) Create a few objects of your generic class with different data types to demonstrate it's
  *          dynamic usage.
  */
-class Gen<T extends Number> {
-    private T ob;
-    private T vals[];
 
-    public Gen(T ob, T[] vals) {
-        this.ob = ob;
+/* Test generic class that takes in an array of any type of Number and performs calculations on it. Interestingly, the
+    class Number does not seem to play well with generics as it is impractical or impossible in some cases to perform
+    certain types of computations with it. Learned a good lesson about simply reading the JavaDoc though, which is how
+    I discovered an elegant solution with the method Number.doubleValue().
+ */
+class Gen<T extends Number> {
+    private T vals[];
+    private T maxValue;
+
+    public Gen(T[] vals, T maxValue) {
         this.vals = vals;
+        this.maxValue = maxValue;
     }
 
+    // Calculates and returns the minimum value in the array.
     public T min() {
-        T min;
+        T min = maxValue;
         for (T val : vals) {
-            if (val < min) {
+            if (val.doubleValue() < min.doubleValue()) {
                 min = val;
             }
         }
         return min;
     }
+
+    // Sorts the array in descending order
+    public void sort() {
+        T placeholder;
+
+        for (int i = 0; i < vals.length - 1; i++) {
+            if (vals[i + 1].doubleValue() < vals[i].doubleValue()) {
+                placeholder = vals[i + 1];
+                vals[i + 1] = vals[i];
+                vals[i] = placeholder;
+            }
+        }
+
+        for (int i = 0; i < vals.length - 1; i++) {
+            if (vals[i].doubleValue() < vals[0].doubleValue()) {
+                placeholder = vals[i];
+                for (int j = i; j > 0; j--) {
+                    vals[j] = vals[j-1];
+                }
+                vals[0] = placeholder;
+            }
+        }
+    }
+
+    // Reverses the array
+    public void reverse() {
+        T placeholder;
+        for (int i = 0; i < vals.length / 2; i++) {
+            placeholder = vals[i];
+            vals[i] = vals[vals.length - i - 1];
+            vals[vals.length - i - 1] = placeholder;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String arrayString = "";
+        for (T val : vals) {
+            arrayString += val + " ";
+        }
+        return arrayString;
+    }
 }
 
 class GenController {
     public static void main(String[] args) {
-        Integer a = {3, 2, 9, 10};
-        Double b = {7.22, 3.17, 0.52};
-        Long c = {719827384739827l, 12346578992989234l, 1283748656988l, 56838289327624l, 195628387492873l};
-        
+        Integer[] a = {7, 2, 5, 0, 6};
+        Double[] b = {7.22, 3.17, 0.52};
+        Long[] c = {719827384739827l, 12346578992989234l, 1283748656988l, 56838289327624l, 195628387492873l};
+
+        Gen<Integer> intArray = new Gen<Integer>(a, Integer.MAX_VALUE);
+        Gen<Double> doubleArray = new Gen<Double>(b, Double.MAX_VALUE);
+        Gen<Long> longArray = new Gen<Long>(c, Long.MAX_VALUE);
+
+        System.out.println(intArray.min());
+        System.out.println(doubleArray.min());
+        System.out.println(longArray.min());
+
+        intArray.sort();
+        doubleArray.sort();
+        longArray.sort();
+
+        System.out.println("\n" + intArray.toString());
+        System.out.println(doubleArray.toString());
+        System.out.println(longArray.toString());
+
+        intArray.reverse();
+        doubleArray.reverse();
+        longArray.reverse();
+
+        System.out.println("\n" + intArray.toString());
+        System.out.println(doubleArray.toString());
+        System.out.println(longArray.toString());
     }
 }
--- src/generics/labs/Exercise_02.java
+++ src/generics/labs/Exercise_02.java
@@ -1,7 +1,49 @@
 package generics.labs;
 
+import java.io.*;
+
 /**
  * Generics Exercise 2:
  *
  *      Create a class with a generic method that takes in an array and prints out the value at a certain index.
  */
+class GenArray {
+    // Prints the array
+    private static <T> void printArray(T[] arr, int index) {
+        System.out.println(arr[index]);
+    }
+
+    // Also trying to practice IO concepts learned (already comfortable with Scanner).
+    public static void main(String[] args) {
+        Integer[] a = {7, 2, 5, 0, 6};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = null;
+        int index = 0;
+        boolean exception = false;
+
+        /* Prompts for and reads user input until no format or out of bounds exceptions are generated, then calls the
+            printArray() method.
+            Inner try/catch catches exceptions related to user input. Outer catches the standard IO exception thrown by
+            readLine(). */
+        do {
+            System.out.println("Choose a number between 0 and " + (a.length - 1));
+            try {
+                try {
+                    // readLine() reads and returns a string value which parseInt() converts into an integer
+                    /* Alternate method read() reads a character value, including numbers, but returns their Unicode
+                        numerical value. */
+                    input = br.readLine();
+                    index = Integer.parseInt(input);
+                    printArray(a, index);
+                    exception = false;
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e1) {
+                    exception = true;
+                }
+            }
+            catch (IOException e2) {
+                // Ignores IO Exception thrown by readLine()
+            };
+        }
+        while (exception == true);
+    }
+}
